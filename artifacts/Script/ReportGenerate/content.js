6566b20a5fb2e704a36965f914026bf3b4064b6a
Ole-Andre Haugen <ole-andre.haugen@neptune-software.com>
2023-01-31 15:21:43 +0100

    Remove illigal characters for reporting

--- artifacts/Script/ReportGenerate/content.js
+++ artifacts/Script/ReportGenerate/content.js
@@ -1,6 +1,17 @@
+let responses = req.body;
+
+// Format illigal characters
+responses.questions.forEach(function (question) {
+    question.responses.forEach(function (response) {
+        response.text = response.text.replaceAll('\t','');
+        response.text = response.text.replaceAll('\b','');
+    });
+});
+
+
 const pdfData = await p9.pdf.generatePdf({
     pdfName: "surveyreport",
-    body: req.body
+    body: responses
 });
 
 result.data = pdfData;
