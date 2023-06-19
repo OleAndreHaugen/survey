f4be946a93f0e98fe2c6ce020c44e8a3b29e169b
Ole Andre <ole-andre.haugen@neptune-software.com>
2023-06-19 09:27:34 +0200

    Possible to override sender eMail

--- artifacts/Script/ReceiversSendMail/content.js
+++ artifacts/Script/ReceiversSendMail/content.js
@@ -1,31 +1,37 @@
 const { In } = operators;
 
 let receiverFilter = req.body.receivers.map(function (receiver) {
-    return receiver.id
+    return receiver.id;
 });
 
 const receivers = await entities.survey_receivers.find({
-    id: In(receiverFilter)
+    id: In(receiverFilter),
 });
 
 const survey = await entities.survey_master.findOne({ id: req.body.surveyid });
 
 receivers.forEach(async function (receiver) {
-
     const link = `${req.body.hostname}/public/app/surveysubmit?surveyid=${survey.id}&link=${receiver.id}`;
 
     let primitives = {
         surveyName: survey.name,
         description: survey.description,
         link: link,
-    }
+    };
 
-    const sendStatus = await sendEmail({
+    let emailOpts = {
         to: receiver.email,
         subject: `${survey.name} Survey`,
         templateId: survey.distribution.emailTemplate,
-        primitives: primitives
-    });
+        primitives: primitives,
+    };
+
+    // Sender eMail address
+    if (survey.distribution.emailSender) {
+        emailOpts.from = survey.distribution.emailSender;
+    }
+
+    const sendStatus = await sendEmail(emailOpts);
 
     if (sendStatus && sendStatus.status === "error") {
         receiver.status = "9";
@@ -34,7 +40,6 @@ receivers.forEach(async function (receiver) {
     }
 
     await entities.survey_receivers.save(receiver);
-
-})
+});
 
 complete();
